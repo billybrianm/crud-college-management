d399886d82a73c1b12be631f97b704a5c7f661a6
billybrian <billybrianm@hotmail.com>
2022-03-08 15:42:30 -0300

    initial commit

---
+++ TechnicalTestMagniFinance/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechnicalTestMagniFinance.Models
+{
+    [Table("Students")]
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Reg_number { get; set; }
+    }
+}
