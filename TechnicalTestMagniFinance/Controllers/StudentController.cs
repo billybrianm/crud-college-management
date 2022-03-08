394261cab11a39a9c2fbaf283e14c2bdadfd1236
billybrian <billybrianm@hotmail.com>
2022-03-08 16:57:03 -0300

    student model and controller

---
+++ TechnicalTestMagniFinance/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechnicalTestMagniFinance.Models;
+
+namespace TechnicalTestMagniFinance.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        // GET Student/GetStudents
+        public JsonResult GetStudents()
+        {
+            using (var db = new MagniFinanceEntities())
+            {
+                List<Student> funcionarios = db.Students.ToList();
+
+                return Json(funcionarios, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
